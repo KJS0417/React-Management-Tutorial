2f9899fc127001cf49b40b38ad766b09f3d58420
Jinsu Kim <kjs417213@naver.com>
2025-07-28 13:46:45 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
 
 const customers = [
   {
@@ -32,9 +38,27 @@ const customers = [
 class App extends Component {
   render() {
     return (
-      <div>
-        {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} gender={c.gender} job={c.job} /> ); }) }
-      </div>
+      <Paper sx={{
+        width: '100%',
+        marginTop: 3,
+        overflowX: "auto"
+      }}>
+        <Table sx={{ minWidth: 1080 }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ); }) }
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
