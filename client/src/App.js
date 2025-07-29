ca943c9d4befafc04d0f08a3c6d77953ab48f761
Jinsu Kim <kjs417213@naver.com>
2025-07-29 15:43:22 +0900

    Add Get Customers REST API Modules

--- client/src/App.js
+++ client/src/App.js
@@ -8,34 +8,26 @@ import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 
-const customers = [
-  {
-  'id': 1,
-  'image': 'https://picsum.photos/seed/woon/64',
-  'name': '김진수',
-  'birthday': '961222',
-  'gender': '남자',
-  'job': '대학생'
-  },
-  {
-  'id': 2,
-  'image': 'https://picsum.photos/seed/picsum/64',
-  'name': '홍길동',
-  'birthday': '960305',
-  'gender': '남자',
-  'job': '프로그래머'
-  },
-  {
-  'id': 3,
-  'image': 'https://picsum.photos/seed/helloworld/64',
-  'name': '이순신',
-  'birthday': '921205',
-  'gender': '남자',
-  'job': '디자이너'
-  }
-]
+
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     return (
       <Paper sx={{
@@ -55,7 +47,7 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ); }) }
+            {this.state.customers ? this.state.customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ); }) : "" }
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({ message: 'Hello Express!' });
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+        'id': 1,
+        'image': 'https://picsum.photos/seed/woon/64',
+        'name': '김진수',
+        'birthday': '961222',
+        'gender': '남자',
+        'job': '대학생'
+        },
+        {
+        'id': 2,
+        'image': 'https://picsum.photos/seed/picsum/64',
+        'name': '홍길동',
+        'birthday': '960305',
+        'gender': '남자',
+        'job': '프로그래머'
+        },
+        {
+        'id': 3,
+        'image': 'https://picsum.photos/seed/helloworld/64',
+        'name': '이순신',
+        'birthday': '921205',
+        'gender': '남자',
+        'job': '디자이너'
+        }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
